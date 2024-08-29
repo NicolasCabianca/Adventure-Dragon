31b5eb2a7fcdd0f26c81ae1dc6d27e66ee307cf9
NicolasCabianca <nicolascabianca7@gmail.com>
2024-08-29 17:17:06 -0300

    Add files via upload

---
+++ outros/gameOver.js
@@ -0,0 +1,23 @@
+var nivel = localStorage.getItem("nivel")
+
+function trocarNivel() {
+    if (nivel == 2) {
+        window.location = "nivel2.html"
+    } else if (nivel == 3) {
+        window.location = "nivel3.html"
+    } else {
+        window.location = "index.html"
+    }
+}
+
+var video
+var sPerdeu
+function preload() {
+    sPerdeu = loadSound("perdeu.mp3")
+}
+function setup() {
+
+
+    sPerdeu.loop()
+    sPerdeu.setVolume(0.1)
+}
